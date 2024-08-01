5c8c3afa993ef65649e44bbcca79b72f938d7187
Khumbolawo <khumbo87@gmail.com>
2024-08-02 00:22:00 +0200

    refactor: :construction: Refactor frontend using django template language

--- smartpharmacist/core/urls.py
+++ smartpharmacist/core/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/',views.login, name='login' ),
+    path('register/',views.register, name='register' ),
+    path('doc-prescription/',views.doc_prescription, name='doc-prescription' ),
+    path('new-patient/',views.create_patient, name='new-patient' ),
+    path('new-prescription/',views.create_prescription, name='new-prescription' ),
+    path('pat-home/',views.patient_home, name='pat-home' ),
+    path('pharm-home/',views.pharmacist_home, name='pharm-home' ),
+    path('new-medication/',views.create_medication, name='new-medication' ),
+
 
 ]
--- smartpharmacist/core/views.py
+++ smartpharmacist/core/views.py
@@ -6,4 +6,29 @@ from django.shortcuts import render
 #     return render(request, "index.html")
 
 def home(request):
-    return render(request, "core/doc-home.html")
+    return render(request, "core/doc-home.html", {'title': 'home'})
+
+def login(request):
+    return render(request, "auth/login.html", {'title': 'login'})
+
+def register(request):
+    return render(request, "auth/register.html", {'title': 'register'})
+
+def doc_prescription(request):
+    return render(request, "core/doc-presc.html", {'title': 'prescriptions'})
+
+def create_patient(request):
+    return render(request, "core/new-patient.html", {'title': 'new patient'})
+
+def create_prescription(request):
+    return render(request, "core/new-prescription.html", {'title': 'new prescription'})
+
+def patient_home(request):
+    return render(request, "core/pat-home.html", {'title': 'home'})
+
+def pharmacist_home(request):
+    return render(request, "core/pharm-home.html", {'title': 'home'})
+
+def create_medication(request):
+    return render(request, "core/new-medication.html", {'title': 'new medication'})
+
