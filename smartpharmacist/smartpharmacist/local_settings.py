a35fbec7c05785e5f18bb4a3a117018b65525aab
Victor Mjimapemba <vcmjimapemba@gmail.com>
2024-08-02 15:19:29 +0200

    editing local_settings

--- smartpharmacist/smartpharmacist/local_settings.py
+++ smartpharmacist/smartpharmacist/local_settings.py
@@ -1,1 +1,19 @@
 from smartpharmacist.base_settings import *
+
+DEBUG = True
+
+DATABASES = {
+    "default":{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smartpharmacist', 
+        'USER':'root',      
+        'PASSWORD': 'mjima',  
+        'HOST': '127.0.0.1',
+        'PORT': '3306',     
+    }
+}
+
+
+
+
+
