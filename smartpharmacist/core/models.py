8d47f5d3e32c5b3632dce3a8369f22d4f70fb4ec
Victor Mjimapemba <vcmjimapemba@gmail.com>
2024-08-02 15:18:44 +0200

    augmenting user model

--- smartpharmacist/core/models.py
+++ smartpharmacist/core/models.py
@@ -1,5 +1,76 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self, username, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(username, password, **other_fields)
+
+    def create_user(self, username, password=None, **other_fields):
+        if not username:
+            raise ValueError(_('You must provide a username'))
+
+        user = self.model(username=username, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ACCOUNT_TYPE_CHOICES = (
+        ('Doctor', 'Doctor'),
+        ('Patient', 'Patient'),
+        ('Pharmacist', 'Pharmacist'),
+    )
+
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    account_type = models.CharField(max_length=150, null=True, choices=ACCOUNT_TYPE_CHOICES)
+    email = models.EmailField(_('email address'), unique=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    national_id = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    dob = models.DateField(_('dob'), null=True)
+    gender = models.CharField(max_length=255, null= True)
+    id_data = models.TextField()
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        if self.first_name:
+            return f"{self.first_name} {self.last_name}"
+        else:
+            return self.username
+
+    def get_fullname(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_absolute_url(self):
+        return reverse("user-detail", kwargs={"pk": self.pk})
 
 class User(AbstractUser):
     is_patient = models.BooleanField(default=False)
