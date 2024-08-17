f5717efaa9509c76f8b63c14f276750a04063a8f
Victor Mjimapemba <vcmjimapemba@gmail.com>
2024-08-18 01:35:22 +0200

    old stable* tailwind setup

--- smartpharmacist/tailwind.config.js
+++ smartpharmacist/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     './core/templates/core/**/*.html',
+    './core/templates/auth/**/*.html',
     './templates/**/*.hmtl',
     './node_modules/flowbite/**/*.js'
     // Add paths to other apps if necessary
