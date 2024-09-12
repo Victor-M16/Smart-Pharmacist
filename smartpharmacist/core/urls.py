ee871b4fd70fbdfc482295b0ef6f70b6788a9e1d
Khumbolawo <khumbo87@gmail.com>
2024-09-12 20:30:54 +0200

    add slot selection  to restock form

--- smartpharmacist/core/urls.py
+++ smartpharmacist/core/urls.py
@@ -8,13 +8,17 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    #template needs reworking
     path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'),
     path('register/',views.CustomRegisterView.as_view(), name='register' ),
 
     #authenticated doctor sees prescriptions
+
+    #doctor urls
     path('doc-prescription/',views.doc_prescription, name='doc-prescription' ),
     path('new-patient/',views.create_patient, name='new-patient' ),
     path('new-prescription/',views.create_prescription, name='new-prescription' ),
+
+    #pharmacist urls
     path('new-medication/',views.create_medication, name='new-medication' ),
+    path('restock/',views.restock, name='restock' ),
 ]
